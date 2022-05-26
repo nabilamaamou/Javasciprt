73d8fc3df11936735bd04db22d7384c40171a623
nabilamaamou <91939153+nabilamaamou@users.noreply.github.com>
2022-05-26 22:44:47 +0200

    Delete ejercicio6 directory

--- ejercicio6/javascript.js
+++
@@ -1,3 +0,0 @@
-Function click() {
-    document.getElementById("palabra1").innerHTML="Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that weve got";
-}
