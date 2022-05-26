c722a6c6fc32366c860a7a6791bafd72652a264d
nabilamaamou <91939153+nabilamaamou@users.noreply.github.com>
2022-05-26 22:42:59 +0200

    Add files via upload

---
+++ ejercicio6/javascript.js
@@ -0,0 +1,3 @@
+Function click() {
+    document.getElementById("palabra1").innerHTML="Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that weve got";
+}
